0325b84750f080522834a6b07a016db0fafaef84
limiaoxing <3377657033@qq.com>
2020-04-10 18:50:15 +0800

    测试

--- src/main/java/com/example/util/ThreadTest.java
+++ src/main/java/com/example/util/ThreadTest.java
@@ -17,7 +17,7 @@ public class ThreadTest implements Runnable{
     public static  Integer Num = 0;
     @Override
     public void run() {
-        //我是李苗星
+        //我是李苗星 ,我会成功的
       //do something here
       for(int i = 0 ; i < 10000;i++){
           System.out.println("线程名称"+Thread.currentThread().getName()+"Num"+Num);
